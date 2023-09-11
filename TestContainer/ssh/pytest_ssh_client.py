f8fdcddc23cec0fa10d5017901d713a64b664bf9
AndreyTokmakov <andtokm@yandex.ru>
2023-09-11 09:54:34 +0400

    11_09_2023__02

--- TestContainer/ssh/pytest_ssh_client.py
+++ TestContainer/ssh/pytest_ssh_client.py
@@ -44,25 +44,23 @@ class TestSSHClient:
         assert self.USER_NAME in output, f'The output shall contain the username'
 
     def test_simple_command_with_timeout(self):
+        start: float = time.time()
         output, code = self.client.exec(cmd='whoami', timeout=5)
+        duration: float = time.time() - start
 
         assert 0 == code, "Call shall succeed"
         assert self.USER_NAME in output, f'The output shall contain the username'
+        assert 0 <= duration <= 1, "It should not take 5 seconds definitely..."
 
     def test_exec_bad_command(self):
         output, code = self.client.exec('some_not_existing_command_i_hope')
         assert 0 != code, "Call shall succeed"
+        assert 'not found' in output
 
     def test_empty_command(self):
         output, code = self.client.exec(cmd='')
+        assert 0 == code, "Call still shall succeed even in case if cmd is empty"
 
-        print('\n', '==' * 100, sep='')
-        print(code)
-        print('\n', '==' * 100, sep='')
-        print(output)
-        print('\n', '==' * 100, sep='')
-
-    '''
     def test_ping(self):
         output, code = self.client.exec('ping localhost -w5')
 
@@ -80,4 +78,3 @@ class TestSSHClient:
 
         assert 5 <= len(pings_send) <= 7, "Expected number of PING's somewhere approximately in range 5 - 7"
         assert 5 <= duration <= 7, "Expected duration somewhere approximately in range 5 - 7 seconds"
-    '''
